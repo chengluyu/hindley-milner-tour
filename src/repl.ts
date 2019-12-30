af13ef504454f4702c6743d89ca8da5e725f6e1f
chengluyu <chengluyu@live.cn>
2019-12-31 00:55:26 +0800

    Add the REPL server class

---
+++ src/repl.ts
@@ -0,0 +1,31 @@
+import { Map } from 'immutable';
+import * as E from './expression';
+import * as T from './type';
+import * as S from './substitution';
+import Annotator from './annotator';
+import Collector from './collector';
+import unify from './unify';
+
+export default class Repl {
+  private valueEnv = Map<string, E.Value>();
+  private typeEnv = Map<string, T.Type>();
+
+  public evaluate(e: E.Expression): [T.Type, E.Value] {
+    const annotator = new Annotator(this.typeEnv);
+    const collector = new Collector(annotator);
+    annotator.visit(e);
+    collector.visit(e);
+    return [
+      S.apply(unify(collector.getEquations()), annotator.getType(e)),
+      e.evaluate(this.valueEnv),
+    ];
+  }
+
+  public declare(declarations: [string, E.Expression][]): void {
+    for (const [name, expression] of declarations) {
+      const [type, value] = this.evaluate(expression);
+      this.valueEnv = this.valueEnv.set(name, value);
+      this.typeEnv = this.typeEnv.set(name, type);
+    }
+  }
+}
